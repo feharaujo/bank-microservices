c7a394a7580a2c07167871a2ce6a49e10d52122c
Felipe T. de Araujo <fetrova@gmail.com>
2025-10-19 14:21:09 +0200

    RabbitMQ with Spring Cloud

--- accounts/src/main/kotlin/com/fetrova/accounts/dto/AccountsContactInfoDto.kt
+++ accounts/src/main/kotlin/com/fetrova/accounts/dto/AccountsContactInfoDto.kt
@@ -3,8 +3,8 @@ package com.fetrova.accounts.dto
 import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties(prefix = "accounts")
-class AccountsContactInfoDto(
-    var message: String? = null,
-    var contactDetails: Map<String, String>? = null,
+class AccountsContactInfoDto {
+    var message: String? = null
+    var contactDetails: Map<String, String>? = null
     var onCallSupport: List<String>? = null
-)
+}
--- cards/src/main/kotlin/com/fetrova/cards/dto/CardsContactInfoDto.kt
+++ cards/src/main/kotlin/com/fetrova/cards/dto/CardsContactInfoDto.kt
@@ -3,8 +3,8 @@ package com.fetrova.cards.dto
 import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties(prefix = "cards")
-class CardsContactInfoDto(
-    var message: String? = null,
-    var contactDetails: Map<String, String>? = null,
+class CardsContactInfoDto {
+    var message: String? = null
+    var contactDetails: Map<String, String>? = null
     var onCallSupport: List<String>? = null
-)
+}
