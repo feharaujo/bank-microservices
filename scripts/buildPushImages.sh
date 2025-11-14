a740804ff795490dbd7ac0ad3581293269c903bd
Felipe T. de Araujo <fetrova@gmail.com>
2025-11-14 10:43:30 +0100

    Gateway Server Docker upload

--- scripts/buildPushImages.sh
+++ scripts/buildPushImages.sh
@@ -1,31 +1,45 @@
 ### Build and Push to docker hub
 # Define the variable at the beginning
-TAG=s8
+TAG=s9
+
+cd ..
 
 cd accounts
+mvn install
 mvn compile jib:dockerBuild
 docker image push docker.io/fearaujo/accounts:$TAG
 
 cd ..
 
 cd cards
+mvn install
 mvn compile jib:dockerBuild
 docker image push docker.io/fearaujo/cards:$TAG
 
 cd ..
 
 cd loans
+mvn install
 mvn compile jib:dockerBuild
 docker image push docker.io/fearaujo/loans:$TAG
 
 cd ..
 
 cd configserver
+mvn install
 mvn compile jib:dockerBuild
 docker image push docker.io/fearaujo/configserver:$TAG
 
 cd ..
 
 cd eurekaserver
+mvn install
 mvn compile jib:dockerBuild
 docker image push docker.io/fearaujo/eurekaserver:$TAG
+
+cd ..
+
+cd gatewayserver
+mvn install
+mvn compile jib:dockerBuild
+docker image push docker.io/fearaujo/gatewayserver:$TAG
